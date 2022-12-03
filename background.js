5f6539a8f979c871a063afccfd8abd214858d122
Erwann Mest <m@kud.io>
2022-12-03 23:46:19 +0000

    ✨ Better icon and handle uk domain

--- background.js
+++ background.js
@@ -1,11 +1,30 @@
-const getHostnameByTab = () => new URL(tab.url).hostname
+/* Handle icon */
+browser.theme.getCurrent().then((theme) => {
+  let iconPath
 
-const getSettingEmailAddress = () => browser.storage.local.get("email").email
+  // Set the icon path based on the current theme
+  if (theme.properties.color_scheme === "dark") {
+    iconPath = "icons/icon-dark.svg"
+  } else {
+    iconPath = "icons/icon.svg"
+  }
+
+  // Set the icon for the current tab
+  browser.browserAction.setIcon({
+    path: iconPath,
+  })
+})
+
+/* Handle Click */
+const getHostnameByTab = (tab) => new URL(tab.url).hostname
+
+const getSettingEmailAddress = () => browser.storage.local.get("email")
 
 const getLabeledEmailAddress = (emailAddress, hostname) => {
   const [preEmail, postEmail] = emailAddress.split("@")
 
-  const fixedHostname = hostname.replace(".uk", "")
+  const fixedHostname =
+    hostname !== "www.gov.uk" ? hostname.replace(".uk", "") : hostname
 
   const hostnameArr = fixedHostname.split(".")
 
@@ -17,7 +36,7 @@ const getLabeledEmailAddress = (emailAddress, hostname) => {
 const handleClick = async (tab) => {
   const hostname = getHostnameByTab(tab)
 
-  const emailAddress = await getSettingEmailAddress()
+  const { email: emailAddress } = await getSettingEmailAddress()
 
   navigator.clipboard.writeText(getLabeledEmailAddress(emailAddress, hostname))
 }
